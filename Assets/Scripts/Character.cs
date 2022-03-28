47757a146eebdf758f248f9f47beb646cb61f792
jomo0825 <jomo0825@gmail.com>
2022-03-29 00:15:07 +0800

    2022.03.29 after class

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -3,66 +3,61 @@ using System.Collections.Generic;
 using UnityEngine;
 using SPStudios.Tools;
 
-[RequireComponent(typeof(CharacterController))]
-public class Character : MonoBehaviour
+namespace MyFramework
 {
-    public float speed = 5.0f;
-    public Animator anim;
-
-    private Vector3 moveVec;
-    private Material mat;
-    private CharacterController cc;
-
-    private void Start()
+    [RequireComponent(typeof(CharacterController))]
+    public class Character : MonoBehaviour
     {
-        mat = GetComponent<Renderer>()?.material;
-        cc = GetComponent<CharacterController>();
+        public float speed = 5.0f;
+        public Animator anim;
+        public CharacterController cc;
 
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
-    }
+        private Vector3 moveVec;
 
-    private void Update()
-    {
-        //Avoid null reference for anim. This happens when you forgot to assign an instance to the variable anim.
-        if (anim != null)
+        private void Start()
         {
-            //Test whether or not the animator is under Locomotion state.
-            //print(anim.GetCurrentAnimatorStateInfo(0).IsName("Locomotion"));
-            
-            anim.SetFloat("speed", moveVec.magnitude);
-            if (moveVec.magnitude > .02f)
+            // Make cursor invisible and lock it at center of screen.
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+
+        private void Update()
+        {
+            //Avoid null reference for anim. This happens when you forgot to assign an instance to the variable anim.
+            if (anim != null)
             {
-                anim.transform.forward = moveVec;
+                //Test whether or not the animator is under Locomotion state.
+                //print(anim.GetCurrentAnimatorStateInfo(0).IsName("Locomotion"));
+
+                if (moveVec.magnitude > .1f)
+                {
+                    anim.transform.forward = moveVec;
+                }
+
+                anim.SetFloat("speed", moveVec.magnitude);
             }
         }
+
+        // Hook/release character motor functions to input manager.
+        private void OnEnable()
+        {
+            // Use singleton plugin.
+            Singletons.Get<MyFramework.InputManager>().IsAxis.AddListener(MoveCharacter);
+        }
+
+        private void OnDisable()
+        {
+            Singletons.Get<MyFramework.InputManager>().IsAxis.RemoveListener(MoveCharacter);
+        }
+
+        // Character motor function.
+        public void MoveCharacter(Vector2 axis)
+        {
+            moveVec.x = axis.x;
+            moveVec.y = 0;
+            moveVec.z = axis.y;
+            //transform.Translate(moveVec * speed * Time.deltaTime, Space.World);
+            cc?.SimpleMove(moveVec * speed);
+        }
     }
-
-    private void OnEnable()
-    {
-        Singletons.Get<MyFramework.InputManager>().IsAxis.AddListener(MoveCharacter);
-        Singletons.Get<MyFramework.InputManager>().OnBtnAttack.AddListener(RandomColor);
-    }
-
-    private void OnDisable()
-    {
-        Singletons.Get<MyFramework.InputManager>().IsAxis.RemoveListener(MoveCharacter);
-        Singletons.Get<MyFramework.InputManager>().OnBtnAttack.RemoveListener(RandomColor);
-    }
-
-    public void MoveCharacter(Vector2 axis)
-    {
-        moveVec.x = axis.x;
-        moveVec.y = 0;
-        moveVec.z = axis.y;
-        //transform.Translate(moveVec * speed * Time.deltaTime, Space.World);
-        cc.SimpleMove(moveVec * speed);
-    }
-
-    public void RandomColor()
-    {
-        mat.color = new Color(Random.value, Random.value, Random.value);
-    }
-
-
 }
